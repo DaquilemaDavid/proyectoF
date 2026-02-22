fce58b03f4548a0900ebabe6b445a5b67886281f
David Jhefferson <jhefferson.daquilema8146@utc.edu.ec>
2026-02-22 11:08:48 -0500

    Configuración de conexión a base de datos y propiedades de JPA

---
+++ src/test/java/com/utc/proyectoF/ProyectoFApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utc.proyectoF;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoFApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
