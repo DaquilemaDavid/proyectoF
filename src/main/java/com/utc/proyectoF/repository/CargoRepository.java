2c957d717dae75780e5711b22075c85b567c4e1a
Israel Trujillo <it231425@gmail.com>
2026-02-22 20:35:50 -0500

    correccion del repository de cargo

--- src/main/java/com/utc/proyectoF/repository/CargoRepository.java
+++ src/main/java/com/utc/proyectoF/repository/CargoRepository.java
@@ -1,7 +1,10 @@
 package com.utc.proyectoF.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import com.utc.proyectoF.entity.Cargo;
 
+@Repository
 public interface CargoRepository extends JpaRepository<Cargo, Long> {
+
 }
