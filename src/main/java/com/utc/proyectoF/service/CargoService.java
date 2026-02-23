6354f0820cef3d7e471cf62b2a8a36c79de81b98
Israel Trujillo <it231425@gmail.com>
2026-02-22 20:34:27 -0500

    creacion del archivo service para cargo

---
+++ src/main/java/com/utc/proyectoF/service/CargoService.java
@@ -0,0 +1,34 @@
+package com.utc.proyectoF.service;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.utc.proyectoF.entity.Cargo;
+import com.utc.proyectoF.repository.CargoRepository;
+
+@Service
+public class CargoService {
+
+    @Autowired
+    private CargoRepository repository;
+
+    // Obtener todos los cargos
+    public List<Cargo> listar() {
+        return repository.findAll();
+    }
+
+    // Buscar un cargo por su ID
+    public Cargo buscarPorId(Long id) {
+        return repository.findById(id).orElse(null);
+    }
+
+    // Guardar o actualizar un cargo
+    public Cargo guardar(Cargo cargo) {
+        return repository.save(cargo);
+    }
+
+    // Eliminar un cargo
+    public void eliminar(Long id) {
+        repository.deleteById(id);
+    }
+}
